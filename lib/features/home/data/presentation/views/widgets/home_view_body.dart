138fb366833a0f3636bc2ffb8d95c9fa7ff26b36
fatmaatef4 <fatmaatef015@gmailcom>
2024-01-08 20:26:36 +0200

    finish splash screen & app bar

---
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_appbar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,75 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:bookly_app/core/utlis/assets_file.dart';
+import 'package:bookly_app/features/home/data/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/slide_text.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: const Duration(seconds: 2),
+      );
+    });
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0.0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Center(
+          child: Image(
+            image: AssetImage(
+              AssetsData.logo,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation)
+      ],
+    );
+  }
+}
