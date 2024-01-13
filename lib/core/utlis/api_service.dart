1e8ea315f47f2f78b44408b5439b7b0651dc9c9b
fatmaatef4 <fatmaatef015@gmailcom>
2024-01-13 17:15:43 +0200

    implement fetchNewest books

--- lib/core/utlis/api_service.dart
+++ lib/core/utlis/api_service.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final _baseUrl = 'https://www.googleapis.com/books/v1/';
   final Dio dio;
-
-  ApiService(this.dio);
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+ApiService(this.dio);
+  
 
   Future<Map<String, dynamic>> getMethod({required String endPoint}) async {
     Response response = await dio.get('$_baseUrl$endPoint');
