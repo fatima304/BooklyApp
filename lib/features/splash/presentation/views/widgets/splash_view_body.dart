53f17b0e2231bb07248ae18d6b9faf9e901a07a3
fatmaatef4 <fatmaatef015@gmailcom>
2024-01-13 13:05:46 +0200

    create book model

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:bookly_app/core/utlis/assets_file.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/slide_text.dart';
