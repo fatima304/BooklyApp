cb1f604a80276a94b01eea20d3a5688e257095ac
fatmaatef4 <fatmaatef015@gmailcom>
2024-01-13 13:37:46 +0200

    commit  create homeRepo methods and error folder

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}
